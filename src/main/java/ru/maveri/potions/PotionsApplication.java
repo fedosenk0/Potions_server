07f539b93c77a9b624887ac1f05a43b56a75e045
Smol <artem.smolonskiy@mail.ru>
2019-10-01 09:39:01 +0300

    схема БД

---
+++ src/main/java/ru/maveri/potions/PotionsApplication.java
@@ -0,0 +1,13 @@
+package ru.maveri.potions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotionsApplication.class, args);
+    }
+
+}
